0c35358063a9fa86a14636e9c562290680c24514
Francisco Hernandez <hpdts@yahoo.com>
2015-05-27 09:37:28 -0300

    adding integration test annotations

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 # Integration Test
 
 http://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-testing.html
+
+# Spring boot
+
+https://spring.io/guides/gs/spring-boot/
+
+Run the app : gradlew build && java -jar build/libs/gs-spring-boot-0.1.0.jar
--- src/e2etest/java/com/hpdts/config/BaseIntegration.java
+++ src/e2etest/java/com/hpdts/config/BaseIntegration.java
@@ -1,28 +1,25 @@
 package com.hpdts.config;
 
 
-import com.hpdts.hello.HelloController;
+import com.hpdts.hello.Application;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.RestTemplate;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = HelloController.class)
+@SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
-@IntegrationTest({"server.port=9090"})
+@IntegrationTest
 public class BaseIntegration {
 
     private String base;
@@ -30,7 +27,7 @@ public class BaseIntegration {
 
     @Before
     public void setUp() throws Exception {
-        this.base = "http://localhost:9090/hello";
+        this.base = "http://localhost:8080";
         template = new TestRestTemplate();
     }
 
