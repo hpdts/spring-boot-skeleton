0987fb66af86b7948185b08d7ebc0af448e85425
unknown <hpdts@yahoo.com>
2015-08-06 22:38:15 -0300

    http angular

--- src/main/resources/admin/js/app.js
+++ src/main/resources/admin/js/app.js
@@ -12,5 +12,14 @@
         this.products = gem;
     });
 
+  app.controller('StoreController',[ '$http', function($http){
+    var store = this;
+    store.products = [];
+
+    $http.get('/products.json').success(function(data){
+      store.products = data;
+    });
+    
+  }]);
 
 })();
