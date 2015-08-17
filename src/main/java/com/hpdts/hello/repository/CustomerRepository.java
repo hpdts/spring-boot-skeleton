e4f3183d3faa4cf57fed6f8271326175409303a3
Francisco Hernandez <hpdts@yahoo.com>
2015-08-17 12:58:44 -0300

    Mongo Db and lombok

---
+++ src/main/java/com/hpdts/hello/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.hpdts.hello.repository;
+
+import com.hpdts.hello.domain.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+    public List<Customer> findByLastName(String lastName);
+
+}
