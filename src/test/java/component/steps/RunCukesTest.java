6223e4628dbad5557d940361ffcdf3d6a4f40cb9
Francisco Hernandez <hpdts@yahoo.com>
2015-09-01 23:17:32 -0300

    fixing cucumber runner

--- src/test/java/component/steps/RunCukesTest.java
+++ src/test/java/component/steps/RunCukesTest.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = "pretty", features = "src/e2etest/resources/")
+@CucumberOptions(format = "pretty", features = "src/test/resources/")
 @Pending
 public class RunCukesTest {
 }
