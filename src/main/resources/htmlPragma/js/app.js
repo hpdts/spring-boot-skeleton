309f4f130f540d77f416b20e8d330c2db28dc552
Francisco Hernandez <hpdts@yahoo.com>
2015-10-23 10:17:30 -0300

    cucumber gradle

--- src/main/resources/htmlPragma/js/app.js
+++ src/main/resources/htmlPragma/js/app.js
@@ -16,7 +16,7 @@
     var store = this;
     store.products = [];
 
-    $http.get('/products.json').success(function(data){
+    $http.get('/banks').success(function(data){
       store.products = data;
     });
     
